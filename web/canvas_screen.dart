e4666d03d31adce5596b6f115b098f3f8eaa8bec
bucho <steelwind666@gmail.com>
2015-02-11 13:10:21 +0900

    リファクタリング

--- web/canvas_screen.dart
+++ web/canvas_screen.dart
@@ -2,6 +2,37 @@ library canvas_screen;
 
 import 'dart:html';
 
+class CanvasScreen {
+  CanvasRenderingContext2D _context;
+  final Font _font;
+  Size _size;
+  String _background_color;
+  CanvasScreen(CanvasElement canvas, Size size, this._font, this._background_color) {
+    _context = canvas.context2D;
+    _size = new Size(_font.width * size.width, _font.height * size.height);
+    canvas
+        ..width = _size.width
+        ..height = _size.height;
+  }
+
+  void clear() {
+    _context
+        ..fillStyle = _background_color
+        ..fillRect(0, 0, _size.width, _size.height);
+  }
+
+  void initialize() {
+    clear();
+    _context.font = _font.family;
+  }
+
+  void write(String text, String color, Coordinate pos) {
+    _context
+        ..fillStyle = color
+        ..fillText(text, pos.x * _font.width, (pos.y + 1) * _font.height);
+  }
+}
+
 class Coordinate {
   int x;
   int y;
@@ -14,37 +45,6 @@ class Font {
   Font(this.family, this._size);
   int get height => _size.height;
   int get width => _size.width;
-}
-
-class CanvasScreen {
-  CanvasRenderingContext2D _context;
-  final Font _font;
-  Size _size;
-  String _background_color;
-  CanvasScreen(CanvasElement canvas, Size size, this._font, this._background_color) {
-    _context = canvas.context2D;
-    _size = new Size(_font.width * size.width, _font.height * size.height);
-    canvas
-      ..width = _size.width
-      ..height = _size.height;
-  }
-
-  void initialize() {
-    clear();
-    _context.font = _font.family;
-  }
-
-  void clear() {
-    _context
-      ..fillStyle = _background_color
-      ..fillRect(0, 0, _size.width, _size.height);
-  }
-
-  void write(String text, String color, Coordinate pos) {
-    _context
-      ..fillStyle = color
-      ..fillText(text, pos.x * _font.width, (pos.y  + 1) * _font.height);
-  }
 }
 
 class Size {
--- web/main.dart
+++ web/main.dart
@@ -1,58 +1,7 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'dart:html';
+
 import 'canvas_screen.dart';
-
-class Game {
-  void render() {}
-  void key_down(int key) {}
-}
-
-class GameRunner {
-  Game _game;
-  GameRunner(this._game);
-  void run() {
-    _game.render();
-    document.body.onKeyDown.listen(_key_down);
-  }
-
-  void _key_down(KeyboardEvent event) {
-    _game.key_down(event.keyCode);
-    _game.render();
-  }
-}
-
-class WalkDemo implements Game {
-  Coordinate _pos;
-  CanvasScreen _screen;
-  WalkDemo(this._screen) {
-    _pos = new Coordinate(3, 3);
-  }
-
-  void key_down(int key) {
-    switch (key) {
-      case 72:
-        _pos.x -= 1;
-        break;
-      case 74:
-        _pos.y += 1;
-        break;
-      case 75:
-        _pos.y -= 1;
-        break;
-      case 76:
-        _pos.x += 1;
-        break;
-    }
-    render();
-  }
-
-  void render() {
-    _screen.clear();
-    _screen.write('@', 'olive', _pos);
-  }
-}
+import 'game_runner.dart';
 
 void main() {
   CanvasElement canvas = new CanvasElement();
@@ -64,4 +13,35 @@ void main() {
   screen.initialize();
   GameRunner runner = new GameRunner(new WalkDemo(screen));
   runner.run();
+}
+
+class WalkDemo implements Game {
+  Coordinate _pos;
+  CanvasScreen _screen;
+  WalkDemo(this._screen) {
+    _pos = new Coordinate(3, 3);
+  }
+
+  void key_down(int key) {
+    switch (key) {
+      case KeyCode.H:
+        _pos.x -= 1;
+        break;
+      case KeyCode.J:
+        _pos.y += 1;
+        break;
+      case KeyCode.K:
+        _pos.y -= 1;
+        break;
+      case KeyCode.L:
+        _pos.x += 1;
+        break;
+    }
+    render();
+  }
+
+  void render() {
+    _screen.clear();
+    _screen.write('@', 'olive', _pos);
+  }
 }
