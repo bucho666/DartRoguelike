af28449a3797e4cf04196d1ad47db0868e4342ff
bucho <steelwind666@gmail.com>
2015-02-11 17:46:34 +0900

    Gridを実装、Fontのサイズが不要になったので削除。

--- web/canvas_screen.dart
+++ web/canvas_screen.dart
@@ -9,7 +9,7 @@ class CanvasScreen {
   String _background_color;
   CanvasScreen(CanvasElement canvas, Size size, this._font, this._background_color) {
     _context = canvas.context2D;
-    _size = new Size(_font.width * size.width, _font.height * size.height);
+    _size = new Size(Grid.width * size.width, Grid.height * size.height);
     canvas
         ..width = _size.width
         ..height = _size.height;
@@ -29,22 +29,50 @@ class CanvasScreen {
   void write(String text, String color, Coordinate pos) {
     _context
         ..fillStyle = color
-        ..fillText(text, pos.x * _font.width, (pos.y + 1) * _font.height);
+        ..fillText(text, pos.x, pos.y + 1);
   }
 }
 
 class Coordinate {
-  int x;
-  int y;
-  Coordinate(this.x, this.y);
+  int _x;
+  int _y;
+  Coordinate(this._x, this._y);
+
+  int get x => _x;
+  int get y => _y;
+
+  Coordinate operator +(Coordinate other) {
+    return new Coordinate(_x + other._x, _y + other._y);
+  }
+}
+
+class Direction {
+  static final Coordinate N = new Coordinate(0, -1);
+  static final Coordinate S = new Coordinate(0, 1);
+  static final Coordinate E = new Coordinate(1, 0);
+  static final Coordinate W = new Coordinate(-1, 0);
+  static final Coordinate NE = new Coordinate(1, -1);
+  static final Coordinate NW = new Coordinate(-1, -1);
+  static final Coordinate SE = new Coordinate(1, 1);
+  static final Coordinate SW = new Coordinate(-1, 1);
 }
 
 class Font {
   final String family;
-  final Size _size;
-  Font(this.family, this._size);
-  int get height => _size.height;
-  int get width => _size.width;
+  Font(this.family);
+}
+
+class Grid extends Coordinate {
+  static Size _size;
+  static get height => _size.height;
+  static set size(Size size) => _size = size;
+  static get width => _size.width;
+  Grid(int x, int y) : super(x, y);
+  int get x => _x * _size.width;
+  int get y => _y * _size.height;
+  Grid operator +(Coordinate other) {
+    return new Grid(_x + other._x, _y + other._y);
+  }
 }
 
 class Size {
--- web/main.dart
+++ web/main.dart
@@ -6,8 +6,9 @@ import 'game_runner.dart';
 void main() {
   CanvasElement canvas = new CanvasElement();
   document.body.children.add(canvas);
+  Grid.size = new Size(10, 16);
   Size screen_size = new Size(80, 24);
-  Font font = new Font("21px 'courier new'", new Size(10, 16));
+  Font font = new Font("22px 'courier new'");
   String background_color = "black";
   CanvasScreen screen = new CanvasScreen(canvas, screen_size, font, background_color);
   screen.initialize();
@@ -18,25 +19,24 @@ void main() {
 class WalkDemo implements Game {
   Coordinate _pos;
   CanvasScreen _screen;
+  Map<int, Coordinate> _dirs;
   WalkDemo(this._screen) {
-    _pos = new Coordinate(3, 3);
+    _pos = new Grid(3, 3);
+    _dirs = {
+      KeyCode.H: Direction.W,
+      KeyCode.J: Direction.S,
+      KeyCode.K: Direction.N,
+      KeyCode.L: Direction.E,
+      KeyCode.Y: Direction.NW,
+      KeyCode.U: Direction.NE,
+      KeyCode.B: Direction.SW,
+      KeyCode.N: Direction.SE,
+    };
   }
 
   void key_down(int key) {
-    switch (key) {
-      case KeyCode.H:
-        _pos.x -= 1;
-        break;
-      case KeyCode.J:
-        _pos.y += 1;
-        break;
-      case KeyCode.K:
-        _pos.y -= 1;
-        break;
-      case KeyCode.L:
-        _pos.x += 1;
-        break;
-    }
+    if (_dirs.containsKey(key) == false) return;
+    _pos += _dirs[key];
     render();
   }
 
