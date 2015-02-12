55dd97ac84563c6f05653d18b22c765695f0c4b5
bucho <steelwind666@gmail.com>
2015-02-13 06:34:22 +0900

    リファクタリング

--- web/game_runner.dart
+++ web/game_runner.dart
@@ -2,21 +2,22 @@ library game_runner;
 
 import 'dart:html';
 
-class Game {
-  void key_down(int key) {}
-  void render() {}
+abstract class Game {
+  void keyDown(int key);
+  void render();
 }
 
 class GameRunner {
-  Game _game;
+  final Game _game;
   GameRunner(this._game);
+
   void run() {
     _game.render();
     document.body.onKeyDown.listen(_key_down);
   }
 
   void _key_down(KeyboardEvent event) {
-    _game.key_down(event.keyCode);
+    _game.keyDown(event.keyCode);
     _game.render();
   }
 }
